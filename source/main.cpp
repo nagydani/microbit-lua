ece9a46a58655dc9f7843665a8ed85034b6c9573
Attila Lendvai <attila@lendvai.name>
2026-05-03 14:40:57 +0200

    cleanup main.cpp

--- source/main.cpp
+++ source/main.cpp
@@ -6,30 +6,17 @@ extern "C" {
 
 #include "MicroBit.h"
 
+// only defined in Lua version >=5.2
+#define LUA_OK 0
+
 MicroBit uBit;
 
 /* -----------------------------
    Lua -> CODAL bridge function
    ----------------------------- */
 
-/*
- * scroll(text)
- * Lua usage: scroll("hello")
- */
 static int l_scroll(lua_State *L) {
-    const char *text = luaL_checkstring(L, 1);
-
-    if (text) {
-        uBit.display.scroll(text);
-    }
-
-    return 0;
-}
-
-/* Panic handler */
-static int panic(lua_State *L) {
-    (void)L;
-    while (1);
+    uBit.display.scroll(luaL_checkstring(L, 1));
     return 0;
 }
 
@@ -38,9 +25,18 @@ static void register_lua_api(lua_State *L) {
     lua_register(L, "scroll", l_scroll);
 }
 
-/* -----------------------------
-   Main entry point
-   ----------------------------- */
+static void idle_loop() {
+  while (1) {
+    uBit.sleep(1000);
+  }
+}
+
+static int lua_panic_handler(lua_State *L) {
+    (void)L;
+    while (1) {
+        uBit.display.scroll("Lua panic");
+    }
+}
 
 int main() {
     uBit.init();
@@ -48,10 +44,10 @@ int main() {
     /* Create Lua state using CODAL heap */
     lua_State *L = luaL_newstate();
     if (!L) {
-        while (1);
+        lua_panic_handler(NULL);
     }
 
-    lua_atpanic(L, panic);
+    lua_atpanic(L, lua_panic_handler);
 
     /* Load standard libraries */
     luaopen_base(L);
@@ -59,15 +55,14 @@ int main() {
     luaopen_string(L);
     luaopen_math(L);
 
-    /* Register CODAL API */
     register_lua_api(L);
 
     /* Example Lua script controlling LEDs */
     const char *script =
-        "scroll('Hello micro:bit')\n"
-        "scroll('Lua + CODAL')\n";
+        "scroll('Lua speaking')\n"
+        "scroll('...')\n";
 
-    if (luaL_loadstring(L, script) || lua_pcall(L, 0, 0, 0)) {
+    if (luaL_dostring(L, script) != LUA_OK) {
         const char *err = lua_tostring(L, -1);
         if (err) {
             uBit.display.scroll("Lua error");
@@ -76,10 +71,7 @@ int main() {
 
     lua_close(L);
 
-    uBit.display.scroll("Lua closed");
+    uBit.display.scroll("Lua exited");
 
-    /* Idle loop */
-    while (1) {
-        uBit.sleep(1000);
-    }
+    idle_loop();
 }
