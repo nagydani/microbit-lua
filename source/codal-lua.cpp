f517b515274318aedee68ccea553c8d1c881ca9e
Daniel A. Nagy <nagy.da@gmail.com>
2026-05-24 22:26:31 +0200

    microbit table with system functions

--- source/codal-lua.cpp
+++ source/codal-lua.cpp
@@ -6,17 +6,42 @@ extern "C" {
 }
 
 #include "MicroBit.h"
+#include "MicroBitDevice.h"
 
 extern MicroBit uBit;
 
-#define LUA_FUNCTION_LIST					\
-    X(sleep,  { int ms = (int)luaL_checkinteger(L, 1);		\
-                uBit.sleep(ms);					\
-                return 0;					\
-              })						\
-    X(scroll, { uBit.display.scroll(luaL_checkstring(L, 1));	\
-                return 0;					\
-              })
+
+
+#define LUA_FUNCTION_LIST						\
+    X(reset,      { uBit.reset();					\
+                    return 0;						\
+                  })							\
+    X(sleep,      { uint32_t ms = (uint32_t)luaL_checkinteger(L, 1);	\
+                    uBit.sleep(ms);					\
+                    return 0;						\
+                  })							\
+    X(seedRandom, { uint32_t seed = (uint32_t)luaL_optinteger(L, 1, 0);	\
+                    if(seed) { uBit.seedRandom(seed); }			\
+                    else { uBit.seedRandom(); }				\
+                    return 0;						\
+		  })							\
+    X(random,     { int max = (int)luaL_checkinteger(L, 1);		\
+                    lua_pushinteger(L, (lua_Integer)uBit.random(max));	\
+                    return 1;						\
+                  })							\
+    X(systemTime, { lua_pushinteger(L, (lua_Integer)uBit.systemTime());	\
+                    return 1;						\
+                  })							\
+    X(friendlyName, { lua_pushstring(L, microbit_friendly_name());	\
+                    return 1;						\
+                  })							\
+    X(panic,      { int statusCode = (int)luaL_checkinteger(L, 1);      \
+                    microbit_panic(statusCode);				\
+                    return 0;						\
+                  })							\
+    X(scroll,     { uBit.display.scroll(luaL_checkstring(L, 1));	\
+                    return 0;						\
+                  })
 
 #define X(name, body) static int l_##name(lua_State *L) body
 LUA_FUNCTION_LIST
--- source/lua-script.lua
+++ source/lua-script.lua
@@ -3,5 +3,5 @@ local uBit = microbit
 uBit.scroll("Lua alive")
 while true do
   uBit.sleep(5000)
-  uBit.scroll("x")
+  uBit.scroll(uBit.friendlyName())
 end
