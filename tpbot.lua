a2fb3fb5bf6aa3807c44aa8d6505215041f26a69
Daniel A. Nagy <nagy.da@gmail.com>
2026-06-27 23:00:34 +0200

    TPBot low-level motor controls (#7)

--- tpbot.lua
+++ tpbot.lua
@@ -10,3 +10,13 @@ end
 function tpbot.set_car_light(r, g, b)
   send("\032"..char(r)..char(g)..char(b))
 end
+
+local function abs(x, n)
+  return math.abs(x), x < 0 and n or 0
+end
+
+function tpbot.set_motors_speed(left, right)
+  local l, d = abs(left, 1)
+  local r, e = abs(right, 2)
+  send("\001"..char(l)..char(r)..char(d + e))
+end
