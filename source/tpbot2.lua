3b5f686366e0ba3b35adef4b5807f65885423428
Daniel A. Nagy <nagy.da@gmail.com>
2026-09-09 10:48:53 +0200

    TPBot sources updated from staging

--- source/tpbot2.lua
+++ source/tpbot2.lua
@@ -1,3 +1,5 @@
+-- Based on https://github.com/elecfreaks/pxt-TPBot/blob/master/V2.ts
+
 local getPin = microbit.io.getPin
 
 tpbot = {
@@ -8,12 +10,13 @@ tpbot = {
 local char = string.char
 local write = microbit.i2c.write
 
-local function send(s)
-  write(32, s)
+local function send(command, params)
+  write(32, "\255\249"..char(command)..
+    char(string.len(params))..params)
 end
 
 function tpbot.set_car_light(r, g, b)
-  send("\032"..char(r)..char(g)..char(b))
+  send(48, char(r)..char(g)..char(b))
 end
 
 local function abs(x, n)
@@ -23,7 +26,7 @@ end
 function tpbot.set_motors_speed(left, right)
   local l, d = abs(left, 1)
   local r, e = abs(right, 2)
-  send("\001"..char(l)..char(r)..char(d + e))
+  send(16, char(l)..char(r)..char(d + e))
 end
 
 local read_digital = microbit.io.getDigitalValue
@@ -36,4 +39,18 @@ function tpbot.get_distance()
   pulse_us(t, 1, 10)
   local r = time_pulse_us(e, 1, 25000)
   return r and r * 0.01715
+end
+
+local function hl(x)
+  local l = x % 256
+  local h = (x - l) / 256
+  return h, l
+end
+
+function tpbot.run_distance(mm)
+  if mm ~= 0 then
+    local d, f = abs(mm, 3)
+    local h, l = hl(d)
+    send(65, char(h)..char(l)..char(f))
+  end
 end
